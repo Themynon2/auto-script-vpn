e47bc1975ea89048bf926fa378d25b86a015fba8
Themynon2 <53654647+Themynon2@users.noreply.github.com>
2019-08-28 12:17:38 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,11 +2,11 @@
 #Menu
 
 echo -e "================================MENU====================================="
-echo -e "* menu       : เรียกดู menu"
-echo -e "* member     : ดูจำนวน user"
-echo -e "* usernew     : สร้าง user ใหม่"
-echo -e "* speedtest     : ทดสอบความเร็วเน็ต"
-echo -e "* userd     : ลบ user"
-echo -e "* trial     : สร้าง user ทดลอง"
+echo -e "* 1: เรียกดู menu"
+echo -e "* 2: ดูจำนวน user"
+echo -e "* 3: สร้าง user ใหม่"
+echo -e "* 4: ทดสอบความเร็วเน็ต"
+echo -e "* 5: ลบ user"
+echo -e "* 6: สร้าง user ทดลอง"
 echo -e "========================================================================="
 echo -e ""
